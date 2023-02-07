7a1006012cef80a8bf36cb3ac51ac9f923ae0b33
lonzofrancisco <lonzofrancisco@gmail.com>
2023-02-06 22:47:22 -0300

    finished

---
+++ Proyecto7bis/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto7bis.Models;
+namespace Proyecto7bis.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            return View(ma.RecuperarTodos());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            Articulo art = ma.Recuperar(id);
+            return View(art);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            Articulo art = new Articulo
+            {
+                Codigo = int.Parse(collection["codigo"]),
+                Descripcion = collection["descripcion"],
+                Precio = float.Parse(collection["precio"].ToString())
+            };
+            ma.Alta(art);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            Articulo art = ma.Recuperar(id);
+            return View(art);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            Articulo art = new Articulo
+            {
+                Codigo = id,
+                Descripcion = collection["descripcion"].ToString(),
+                Precio = float.Parse(collection["precio"].ToString())
+            };
+            ma.Modificar(art);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            Articulo art = ma.Recuperar(id);
+            return View(art);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            ma.Borrar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Proyecto7bis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto7bis.MvcApplication" Language="C#" %>
